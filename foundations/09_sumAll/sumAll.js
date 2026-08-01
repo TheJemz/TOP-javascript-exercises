bd03d2506c329dce39c067bd4312aa1bdfaba38a
Cipher Void <260451390+TheJemz@users.noreply.github.com>
2026-08-01 17:54:41 +0500

    Complete Data Types and Conditionals exercises 08-11
    Implemented array filtering, sum range, leap year
    checker and temperature conversion.

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,4 +1,17 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    
+    if (typeof a !== 'number' || !Number.isInteger(a) || a < 0 || typeof b !== 'number' || !Number.isInteger(b) || b < 0) {
+        return 'ERROR';
+    } else {
+        let min = Math.min (a, b);
+        let max = Math.max (a, b);
+        let sum = 0;
+
+        for (let i = min; i <= max; i++) {
+            sum = sum + i;
+        }
+        return sum;
+    }
 
 };
 
