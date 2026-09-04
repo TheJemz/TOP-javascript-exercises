01d9558979b7ef614c9224f0af2f43bf2def1bf0
Cipher Void <260451390+TheJemz@users.noreply.github.com>
2026-09-04 14:46:39 +0500

    Complete exercises 12-13; calculator and palindrome checker
    Calculator covers six math operations. Palindrome
    cleans input with regex and compares original
    against reversed string to verify it.

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;	
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;	
 };
 
-const sum = function() {
-	
+const sum = function(num) {
+  const sumAll = num.reduce( (total, cur) => {
+    return total + cur;
+  }, 0 );
+  return sumAll;
 };
 
-const multiply = function() {
-
+const multiply = function(num) {
+  const mul = num.reduce( (total, cur) => {
+    return total * cur;
+  }, 1);
+  return mul;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  const fact = Array.from( {length: num}, (_, i) => i + 1 );
+  return fact.reduce( (fac, cur) => fac * cur, 1 );
 };
 
 // Do not edit below this line
--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (phrase) {
+    const clean = phrase.toLowerCase().replace(/[^a-z0-9]/g, '')
+    const reversed = clean.split('').reverse().join('');
+    return clean === reversed;
 };
 
 // Do not edit below this line
