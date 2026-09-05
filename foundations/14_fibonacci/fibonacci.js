02f88c666f144573d5d38a27f119c5ec7c0665af
Cipher Void <260451390+TheJemz@users.noreply.github.com>
2026-09-05 19:07:47 +0500

    Complete Fibonacci exercise
    Implement a function that returns the specified
    Fibonacci number using an iterative approach.
    Handle input validation for zero, negative inputs,
    and other data types according to the exercise
    requirements.

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,34 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+
+  let prv = 0;
+  let cur = 1;
+  let next;
+
+  let count;
+
+  if (typeof index !== 'number' ) {
+    count = parseInt(index);
+  } else {
+    count = index;
+  };
+
+  if ( count < 0 ) return 'OOPS';
+  if ( count == 0 ) return 0 ;
+
+
+  for (let i = 0; i < (count - 1); i++) {
+
+    next = prv + cur;
+    prv = cur;
+    cur = next;
+    
+  };
+
+  return cur;
 
 };
+
+
 
 // Do not edit below this line
 module.exports = fibonacci;
