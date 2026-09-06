f568cd56232ff8a26b1020126ab01cf4b3beb52e
Cipher Void <260451390+TheJemz@users.noreply.github.com>
2026-09-06 22:22:17 +0500

    Complete exercise 16; find oldest person
    Calculates each person's lifespan using reduce,
    handles undefined death years with today's year to
    ensuring living persons are considered.

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,4 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+
+    const today = new Date().getFullYear();
+
+    const oldest = people.reduce( (older, current) => {
+
+        let curDeathYear = current.yearOfDeath || today;
+        let oldDeathYear = older.yearOfDeath || today;
+
+        const old = oldDeathYear - older.yearOfBirth;
+        const cur = curDeathYear - current.yearOfBirth;
+
+
+        return old > cur ? older : current;
+
+    } );
+
+    return oldest;
 
 };
 
